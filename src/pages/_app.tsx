4479a8373c017786e0530b81dafed91476bbc012
Ruy Monteiro <ruystfrancis@gmail.com>
2022-02-17 20:43:45 -0300

    Fetch data server side

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,6 @@ function MyApp({ Component, pageProps }) {
   / Validate if a query was made in Server side
   / and if it's valid to be reused in the client.
   */
- 
   if (pageProps.urqlState) {
     ssrCache.restoreData(pageProps.urqlState);
   }
