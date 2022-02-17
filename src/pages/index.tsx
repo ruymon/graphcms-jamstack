bc6fed8964bebcbead6e9a9cf63f9743ea5ee189
Ruy Monteiro <ruystfrancis@gmail.com>
2022-02-17 20:38:14 -0300

    Setup GraphQL queries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import { usePageQuery } from "../generated/graphql"
+
 export default function Home() {
+  const [{ data }] = usePageQuery({
+    variables: {
+      slug: 'home'
+    }
+  })
+  
   return (
-    <h1>Hello World</h1>
+    <h1>{data?.page.title}</h1>
   )
 }
