f51736e8632d72818b423e251d25398888768a65
Ruy Monteiro <ruystfrancis@gmail.com>
2022-02-17 20:08:14 -0300

    Add urql provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
+import { Provider } from 'urql';
+import { client, ssrCache } from '../lib/urql';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  /*
+  / Validate if a query was made in Server side
+  / and if it's valid to be reused in the client.
+  */
+ 
+  if (pageProps.urqlState) {
+    ssrCache.restoreData(pageProps.urqlState);
+  }
+
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp
